9b9d840f3b5d51191cd3056966f957b0dd13afee
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2021-02-06 19:39:03 +0200

    add files and dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FilesComponent } from './components/files/files.component';
+import { EmployeesComponent } from './components/employees/employees.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SearchEmployeeComponent } from './components/employees/search-employee/search-employee.component';
+import { AddEmployeeComponent } from './components/employees/add-employee/add-employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/employees', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'files', component: FilesComponent },
+  {
+    path: 'employees',
+    component: EmployeesComponent,
+    children: [
+      { path: 'addEmployee', component: AddEmployeeComponent },
+      { path: 'searchEmployee', component: SearchEmployeeComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
+
 // component if our app
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { AddEmployeeComponent } from './components/employees/add-employee/add-employee.component';
+import { SearchEmployeeComponent } from './components/employees/search-employee/search-employee.component';
+import { FilesComponent } from './components/files/files.component';
 // modules from angular material
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
-import { SearchEmployeeComponent } from './components/search-employee/search-employee.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { EmployeesComponent } from './components/employees/employees.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, NavigationComponent, AddEmployeeComponent, SearchEmployeeComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    AddEmployeeComponent,
+    SearchEmployeeComponent,
+    DashboardComponent,
+    EmployeesComponent,
+    FilesComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -25,6 +38,7 @@ import { SearchEmployeeComponent } from './components/search-employee/search-emp
     MatToolbarModule,
     MatInputModule,
     FontAwesomeModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
